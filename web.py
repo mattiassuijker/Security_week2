7c4997772d8866e6e91430fcf63118db8510552c
1027048-Yassine <1027048@hr.nl>
2022-11-30 14:22:23 +0100

    homepagina, aanpas/verwijder buttons toegevoegd

--- web.py
+++ web.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("test-correct.html")
+    return "mo gemaakt"
 
 if __name__ == "__main__":
     app.run(debug = True)
