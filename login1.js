2974272b43e103ef15af676ab44bc0b8491cc81f
1012625 <80328925+1012625@users.noreply.github.com>
2022-12-04 03:07:33 +0100

    admin account werkt soort van

--- login1.js
+++ login1.js
@@ -4,11 +4,12 @@ var username=document.getElementById("username").Value;
 var password=document.getElementById("password").Value;
 if(username == "admin" && password == "user")
 {
+    window.location.assign("home.html");
     alert("Login is Succesvol");
-    return false;
 }
 else
 {
     alert("fout wachtwoord of gebruikersnaam");
+    return;
 }
 }
